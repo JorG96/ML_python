9d54c5e855211287f8a6911a49bd3b9acb4e1990
JorG96 <jecamg96@gmail.com>
2021-12-18 13:25:09 -0400

    salary input output split

--- Salary.py
+++ Salary.py
@@ -1,30 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Dec  6 12:46:49 2020
-
-@author: Alberto Barbado Gonzalez
-"""
-
-# Importar librerias
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
 # =============================================================================
 # Data Preprocessing
 # =============================================================================
-# Cargar dataset
+# read dataset with pandas
 df = pd.read_csv('Salary_Data.csv')
 
-# Visualizar los datos
-df.plot(x='YearsExperience', y='Salary', title="Evolucion del Salario segun los Años de Experiencia") # Se ve relacion lineal
+# visualization
+df.plot(x='YearsExperience', y='Salary', title="Salary evolution per years of experience")
 
-# Separacion en variables entrada/salida
-X = df.iloc[:, :-1].values
+# i/o split values
+x = df.iloc[:, :-1].values
 y = df.iloc[:, 1].values
 
 # Train/Test Split
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 1/3, random_state = 0)
 
 
+import statsmodels.api as sm
+x_train = sm.add_constant(x_train) # b0
+x_test = sm.add_constant(x_test) # b0
+# model = sm.OLS(y_train, X_train).fit()
+# model.summary()
