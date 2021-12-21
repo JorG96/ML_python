a30d7b320964a11b7c04d9097ca95644bdf069fd
JorG96 <jecamg96@gmail.com>
2021-12-20 22:19:17 -0400

    Salary: model train and test

--- Salary.py
+++ Salary.py
@@ -44,3 +44,51 @@ print(r**2)
 # Test D'Agostino
 from scipy.stats import normaltest
 normaltest(residuals)
+
+# Check Homoscedasticity
+# Goldfeld-Quandt test 
+name = ['F statistic', 'p-value']
+test = sms.het_goldfeldquandt(model.resid, model.model.exog)
+lzip(name, test)
+
+# checking homoscedasticity with a plot
+fig, ax = plt.subplots(figsize=(6,2.5))
+_ = ax.scatter(x_train, residuals)
+
+#residuals autocorrelation with Durbin-Watson
+from statsmodels.stats.stattools import durbin_watson
+print(durbin_watson(residuals))
+
+#leverage
+from statsmodels.stats.outliers_influence import OLSInfluence
+test_class = OLSInfluence(model)
+test_class.dfbetas[:5,:]
+
+from statsmodels.graphics.regressionplots import plot_leverage_resid2,influence_plot
+fig, ax = plt.subplots(figsize=(8,6))
+fig = plot_leverage_resid2(model, ax = ax)
+influence_plot(model)
+
+#model's prediction
+y_pred = model.predict(x_test)
+
+#Metrics evaluation
+from sklearn.metrics import mean_squared_error, r2_score
+r2 = r2_score(y_test, y_pred)
+mae = mean_squared_error(y_test, y_pred)
+print("r2: ", r2, "mae: ", mae)
+
+# Visualizing the Training results
+plt.scatter(x_train, y_train, color = 'red')
+plt.plot(x_train, model.predict(x_train), color = 'blue')
+plt.title('Salary vs Esperience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+# Visualizing the Test results
+plt.scatter(x_test, y_test, color = 'red')
+plt.plot(x_train, model.predict(x_train), color = 'blue')
+plt.title('Salary vs Esperience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
