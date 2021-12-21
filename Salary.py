40e1afd60805e9bec6ec672fa8f251bc82d6d75f
JorG96 <jecamg96@gmail.com>
2021-12-20 20:23:03 -0400

    Salary: residuals calculation

--- Salary.py
+++ Salary.py
@@ -21,7 +21,26 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 1/3, rando
 
 
 import statsmodels.api as sm
-x_train = sm.add_constant(x_train) # b0
-x_test = sm.add_constant(x_test) # b0
-# model = sm.OLS(y_train, X_train).fit()
-# model.summary()
+# x_train = sm.add_constant(x_train) # b0
+# x_test = sm.add_constant(x_test) # b0
+model = sm.OLS(y_train, x_train).fit()
+model.summary()
+
+# test linear model
+import statsmodels.stats.api as sms
+sms.linear_harvey_collier(model)
+
+# calculate residuals
+from statsmodels.compat import lzip
+residuals = model.resid
+plt.hist(residuals, range=(-45000, 45000))
+
+# Q-Q plot
+import scipy as sp
+fig, ax = plt.subplots(figsize=(6,2.5))
+_, (__, ___, r) = sp.stats.probplot(residuals, plot=ax, fit=True)
+print(r**2)
+
+# Test D'Agostino
+from scipy.stats import normaltest
+normaltest(residuals)
