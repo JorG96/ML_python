7a795b1d8af87e050b6f4ce390d219e51dc7972c
JorG96 <jecamg96@gmail.com>
2022-01-24 11:35:57 -0400

    Caso_Wine first commit

---
+++ Caso_Wine.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 10:43:23 2022
+
+@author: ASUS
+"""
+# 1. ANALISIS EXPLORATORIO
+#   Librerias
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Cargar Datos
+df = pd.read_csv("Caso_Wine.csv")
+df.head()
+
+# 2. MODELO DE CLUSTERING
+
+
+# 3. MODELO PREDICTIVO
+
+# PCA
