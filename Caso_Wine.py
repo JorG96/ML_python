0cb62d41d41c4d13bcc20a6100ad8891347fd568
JorG96 <jecamg96@gmail.com>
2022-01-26 22:15:36 -0400

    Caso_Wine: EDA

--- Caso_Wine.py
+++ Caso_Wine.py
@@ -13,9 +13,28 @@ import seaborn as sns
 
 # Cargar Datos
 df = pd.read_csv("Caso_Wine.csv")
-df.head()
-
+print(df.columns)
+# Identificacion de variables
+df.info()
+df.describe()
+# Grafico de densidad
+fig, ax = plt.subplots(5,3, figsize=(14,12))
+axes_ = [axes_row for axes in ax for axes_row in axes]
+for i,c in enumerate(df.columns):
+    sns.distplot(df[c], ax = axes_[i], color = 'Set1')
+    plt.tight_layout()
+# Graficos de caja
+fig, ax = plt.subplots(5,3, figsize=(14,12))
+axes_ = [axes_row for axes in ax for axes_row in axes]
+for i,c in enumerate(df.columns):
+    sns.boxplot(df[c], ax = axes_[i], palette='Set1')
+    plt.tight_layout()
+# Matriz de correlacion
+f,ax = plt.subplots(figsize=(20,20))
+sns.heatmap(df.corr(method='spearman'),annot=True,fmt=".1f",linewidths=1,ax=ax)
+plt.show()
 # 2. MODELO DE CLUSTERING
+# Columnas
 
 
 # 3. MODELO PREDICTIVO
