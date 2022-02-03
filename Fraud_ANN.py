09494ac548c947ccd608e2155b474360f24dab39
JorG96 <jecamg96@gmail.com>
2022-02-03 18:43:06 -0400

    Fraud_ANN: Data Preparation

--- Fraud_ANN.py
+++ Fraud_ANN.py
@@ -16,14 +16,27 @@ import math
 # 1 Data Preparation
 dataset = pd.read_csv('paysim_reduced.csv')
 
+df_aux = pd.get_dummies(dataset['type']).astype('int')
+dataset.drop(['type', 'Unnamed: 0', 'nameOrig',
+ 'nameDest', 'isFlaggedFraud', 'step',
+ 'newbalanceOrig', 'newbalanceDest'], axis=1, inplace=True)
+dataset = dataset.join(df_aux)
+X = dataset.loc[:, dataset.columns != 'isFraud'].values
+y = dataset['isFraud'].values
 
 # Train/Test
 
+from sklearn.model_selection import train_test_split
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25,
+random_state = 0)
 
 # Feature Scaling
 
-
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
 
 # 2 ANN Build 
