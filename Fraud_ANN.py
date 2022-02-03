c528e60343019dc09e17a7c90801ca52c4dbfeb8
JorG96 <jecamg96@gmail.com>
2022-02-03 18:31:45 -0400

    Fraud_ANN: first commit

---
+++ Fraud_ANN.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  3 18:25:48 2022
+
+@author: ASUS
+"""
+# Dataset https://www.kaggle.com/ealaxi/paysim1
+
+# Librerías
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+
+
+# 1 Data Preparation
+dataset = pd.read_csv('paysim_reduced.csv')
+
+
+# Train/Test
+
+
+
+# Feature Scaling
+
+
+
+
+# 2 ANN Build 
+
+# Importing the Keras libraries and packages
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import tensorflow as tf
+
+# 3 ANN Predictions
