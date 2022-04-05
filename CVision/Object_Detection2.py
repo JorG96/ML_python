b55be804d45984eddea8fd727525fc0e6c4d2f91
Jorge Camacho <Jorge.Camacho1@niceincontact.com>
2022-04-05 17:39:54 -0400

    .gitignore and CVision folder updated

---
+++ CVision/Object_Detection2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 17:15:29 2022
+
+@author: Jorge.Camacho1
+"""
+
+import cv2
+import matplotlib.pyplot as plt
+import cvlib as cv
+from cvlib.object_detection import draw_box
+
