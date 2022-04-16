fbed78789330acd8d89e48204b5a22e24b5088dc
JorG96 <jecamg96@gmail.com>
2022-04-16 19:32:45 -0400

    yolo object detection update

--- CVision/Object_Detection_Classification.py
+++ CVision/Object_Detection_Classification.py
@@ -6,18 +6,25 @@ Created on Mon Apr 11 10:34:39 2022
 """
 #libraries
 import numpy as np
+import imutils
 import argparse
 import time
 import cv2
 import os
 
 #parse arguments
-ap=argparse.ArgumentParser()
-ap.add_argument("-i", "--image",required=True,help="path to input image")
-ap.add_argument("-y", "--yolo",required=True,help="base path to YOLO directory")
-ap.add_argument("-c", "--confidence",type=float,default=0.5,help="minimum probability to filter weak detections")
-ap.add_argument("-t", "--threshold",type=float,default=0.3,help="threshold when apllying non-maxima suppression")
-args= vars(ap.parse_args())
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True,
+    help="path to input video")
+ap.add_argument("-o", "--output", required=True,
+    help="path to output video")
+ap.add_argument("-y", "--yolo", required=True,
+    help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+    help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+    help="threshold when applyong non-maxima suppression")
+args = vars(ap.parse_args())
 
 #load COCO class labels
 labelsPath=os.path.sep.join([args["yolo"],"coco.names"])
@@ -29,57 +36,29 @@ COLORS=np.random.randint(0,255,size=(len(LABELS),3),dtype="uint8")
 # derive the paths to the YOLO weights and model configuration
 weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
 configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
-# load our YOLO object detector trained on COCO dataset (80 classes)
+# load our YOLO object detector trained on COCO dataset
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
-
-
-# load our input image and grab its spatial dimensions
-image = cv2.imread(args["image"])
-(H, W) = image.shape[:2]
-# determine only the *output* layer names that we need from YOLO
 ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-# construct a blob from the input image and then perform a forward
-# pass of the YOLO object detector, giving us our bounding boxes and
-# associated probabilities
-blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
-	swapRB=True, crop=False)
-net.setInput(blob)
-start = time.time()
-layerOutputs = net.forward(ln)
-end = time.time()
-# show timing information on YOLO
-print("[INFO] YOLO took {:.6f} seconds".format(end - start))
-# initialize our lists of detected bounding boxes, confidences, and
-# class IDs, respectively
-boxes = []
-confidences = []
-classIDs = []
-# loop over each of the layer outputs
-for output in layerOutputs:
-	# loop over each of the detections
-	for detection in output:
-		# extract the class ID and confidence (i.e., probability) of
-		# the current object detection
-		scores = detection[5:]
-		classID = np.argmax(scores)
-		confidence = scores[classID]
-		# filter out weak predictions by ensuring the detected
-		# probability is greater than the minimum probability
-		if confidence > args["confidence"]:
-			# scale the bounding box coordinates back relative to the
-			# size of the image, keeping in mind that YOLO actually
-			# returns the center (x, y)-coordinates of the bounding
-			# box followed by the boxes' width and height
-			box = detection[0:4] * np.array([W, H, W, H])
-			(centerX, centerY, width, height) = box.astype("int")
-			# use the center (x, y)-coordinates to derive the top and
-			# and left corner of the bounding box
-			x = int(centerX - (width / 2))
-			y = int(centerY - (height / 2))
-			# update our list of bounding box coordinates, confidences,
-			# and class IDs
-			boxes.append([x, y, int(width), int(height)])
-			confidences.append(float(confidence))
-			classIDs.append(classID)
+
+# initialize the video stream, pointer to output video file, and
+# frame dimensions
+vs = cv2.VideoCapture(args["input"])
+writer = None
+(W, H) = (None, None)
+ 
+# try to determine the total number of frames in the video file
+try:
+    prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
+        else cv2.CAP_PROP_FRAME_COUNT
+    total = int(vs.get(prop))
+    print("[INFO] {} total frames in video".format(total))
+ 
+# an error occurred while trying to determine the total
+# number of frames in the video file
+except:
+    print("[INFO] could not determine # of frames in video")
+    print("[INFO] no approx. completion time can be provided")
+    total = -1
+    
